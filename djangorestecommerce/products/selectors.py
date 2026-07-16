3f86fd3efa122055c7d3d4aa1ca1579fc57b89b8
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-16 19:15:55 +0330

    add get_product_by_slug and get_all_products for return in apis.py

--- djangorestecommerce/products/selectors.py
+++ djangorestecommerce/products/selectors.py
@@ -8,4 +8,10 @@ def get_category_by_slug(slug: str) -> Category:
     return get_object_or_404(Category, slug=slug) 
 
 def get_all_categories() -> QuerySet[Category]: 
-    return Category.objects.all()
+    return Category.objects.all() 
+
+def get_product_by_slug(slug: str) -> Product: 
+    return get_object_or_404(Product, slug=slug) 
+
+def get_all_products() -> QuerySet[Product]:
+    return Product.objects.all() 

34c11d27456f7c0af398a8d6566af867ad38eee7
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-16 19:16:47 +0330

    add product-detail & product-list

--- djangorestecommerce/products/urls.py
+++ djangorestecommerce/products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include 
 from djangorestecommerce.products.apis import (
-    CategoryApiView
+    CategoryApiView, ProductApiView
 )
 
 urlpatterns = [
+    path("", ProductApiView.as_view(), name="product-list"),
+    path("<slug:slug>/", ProductApiView.as_view(), name="product-detail"),
     path("categories/", CategoryApiView.as_view(), name="category-list"),
     path("categories/<slug:slug>/", CategoryApiView.as_view(), name="category-detail"),
     

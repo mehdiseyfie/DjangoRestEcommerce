7e2143a71ccb520080c0169d5c2f93e269fad778
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-16 19:14:30 +0330

    add ProductApiView with get()

--- djangorestecommerce/products/apis.py
+++ djangorestecommerce/products/apis.py
@@ -6,7 +6,10 @@ from djangorestecommerce.products.models import(
 )
 from drf_spectacular.utils import extend_schema 
 from djangorestecommerce.products.selectors import (
-    get_category_by_slug, get_all_categories
+    get_category_by_slug, 
+    get_all_categories, 
+    get_product_by_slug,
+    get_all_products
 )
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication 
@@ -17,7 +20,7 @@ class CategoryApiView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
-    class OutputCategory(serializers.ModelSerializer): 
+    class OutputCategorySerializer(serializers.ModelSerializer): 
         class Meta: 
             model = Category 
             fields = (
@@ -26,21 +29,53 @@ class CategoryApiView(APIView):
                 "description",
                 
             ) 
-    @extend_schema(responses=OutputCategory())
+    @extend_schema(responses=OutputCategorySerializer())
     def get(self, request, slug=None): 
         if slug: 
             category = get_category_by_slug(slug=slug) 
-            serializer = self.OutputCategory(
+            serializer = self.OutputCategorySerializer(
                 category, context={"request", request}
             )
             return Response(serializer.data, status=status.HTTP_200_OK) 
         else: 
             categories = get_all_categories()
-            serializer = self.OutputCategory(
+            serializer = self.OutputCategorySerializer(
                 categories, many=True, context={"request":request}
             )
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ProductApiView(APIView):
+    permission_classes = [IsAuthenticated] 
+    authentication_classes = [JWTAuthentication] 
+    
+    class OutputProductSerializer(serializers.ModelSerializer): 
+        category = serializers.CharField(source="category.name")
         
+        class Meta: 
+            model = Product 
+            fields = "__all__" 
+    
+    @extend_schema(responses=OutputProductSerializer) 
+    def get(self, request, slug=None):
+        if slug: 
+            product = get_product_by_slug(slug=slug) 
+            serializer = self.OutputProductSerializer(
+                product, context={"request", request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            products = get_all_products() 
+            serializer = self.OutputProductSerializer(
+                products, many=True, context={"request":request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK) 
+        
+            
+            
+            
+            
+            
+
         
         
         

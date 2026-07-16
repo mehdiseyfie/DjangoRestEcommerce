b9db4b09e73504c0c7416b4f8ad8149692d8a03d
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-16 17:09:04 +0330

    add products in api/urls.py

--- djangorestecommerce/api/urls.py
+++ djangorestecommerce/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path("authentication/", include(("djangorestecommerce.authentication.urls", "authentication"))),
     path("users/", include(("djangorestecommerce.users.urls", "users"))),
+    path("products/", include(("djangorestecommerce.products.urls", "products"))),
     
 ]

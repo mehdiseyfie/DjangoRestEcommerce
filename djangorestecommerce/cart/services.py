227c472dc90d3b508e402dd7548b92f66b5e2ca7
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-19 18:18:53 +0330

    add update_cart_item function for updated_item in apis

--- djangorestecommerce/cart/services.py
+++ djangorestecommerce/cart/services.py
@@ -2,15 +2,16 @@ from djangorestecommerce.users.models import Profile
 from djangorestecommerce.cart.models import Cart, CartItem
 from djangorestecommerce.products.models import Product
 from django.core.exceptions import ValidationError
+from django.db import transaction
 
-
+@transaction.atomic
 def get_cart_or_create(customer: Profile) -> Cart: 
     try: 
         return Cart.objects.get(customer=customer) 
     except Cart.DoesNotExist: 
         return Cart.objects.create(customer=customer) 
 
-
+@transaction.atomic
 def add_item_to_cart(cart: Cart, product: Product, quantity: int) -> CartItem: 
     if quantity <= 0:
         raise ValidationError("Quantity must be positive")
@@ -28,4 +29,26 @@ def add_item_to_cart(cart: Cart, product: Product, quantity: int) -> CartItem:
     except CartItem.DoesNotExist: 
         cart_item = CartItem.objects.create(cart=cart, product=product, quantity=quantity)  
         
-    return cart_item
+    return cart_item 
+
+
+@transaction.atomic
+def update_cart_item(
+            item: CartItem,
+            quantity: int
+            ) -> CartItem: 
+    if quantity <= 0:
+        raise ValidationError("Quantity must be positive") 
+    if item.product.stock < quantity:
+        raise ValidationError(f"Insufficient stock. Available: {item.product.stock}")  
+    
+    old_quantity = item.quantity 
+    old_price = item.price 
+    
+    item.quantity = quantity 
+    
+    item.save(old_quantity=old_quantity, old_price=old_price)
+    
+    return item
+
+

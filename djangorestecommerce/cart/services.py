903ec6e635284aba11f3e2cc966030fcc98b179e
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-19 12:57:46 +0330

    add_item_to_cart() function for add Item

--- djangorestecommerce/cart/services.py
+++ djangorestecommerce/cart/services.py
@@ -1,9 +1,31 @@
 from djangorestecommerce.users.models import Profile 
-from djangorestecommerce.cart.models import Cart 
+from djangorestecommerce.cart.models import Cart, CartItem
+from djangorestecommerce.products.models import Product
+from django.core.exceptions import ValidationError
 
 
 def get_cart_or_create(customer: Profile) -> Cart: 
     try: 
         return Cart.objects.get(customer=customer) 
     except Cart.DoesNotExist: 
-        return Cart.objects.create(customer=customer)
+        return Cart.objects.create(customer=customer) 
+
+
+def add_item_to_cart(cart: Cart, product: Product, quantity: int) -> CartItem: 
+    if quantity <= 0:
+        raise ValidationError("Quantity must be positive")
+    
+    if product.stock < quantity:
+        raise ValidationError(f"Insufficient stock. Available: {product.stock}")
+    try: 
+        cart_item = CartItem.objects.get(cart=cart, product=product) 
+        old_quantity = cart_item.quantity 
+        old_price = cart_item.price 
+        total_quantity = old_quantity + quantity 
+        cart_item.quantity = total_quantity
+        cart_item.save(old_quantity=old_quantity, old_price=old_price) 
+        
+    except CartItem.DoesNotExist: 
+        cart_item = CartItem.objects.create(cart=cart, product=product, quantity=quantity)  
+        
+    return cart_item

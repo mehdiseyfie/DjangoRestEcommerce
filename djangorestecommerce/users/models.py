e984dc45625da66d17d4bfd7c92bba59ec95c7b4
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-29 11:10:11 +0330

    add ShippingAddress in models.py

--- djangorestecommerce/users/models.py
+++ djangorestecommerce/users/models.py
@@ -3,7 +3,8 @@ from djangorestecommerce.common.models import BaseModel
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager as BUM
 from django.contrib.auth.models import PermissionsMixin 
-from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.modelfields import PhoneNumberField 
+from django.utils.translation import gettext_lazy as _
 
 
 
@@ -11,7 +12,7 @@ class BaseUserManager(BUM):
     def create_user(
                         self, first_name, last_name, phone,
                         email, is_active=True,is_admin=False,
-                        password=None, address=None
+                        password=None
                     ):
         if not email:
             raise ValueError("Users must have an email address") 
@@ -23,7 +24,7 @@ class BaseUserManager(BUM):
                             first_name=first_name,
                             last_name=last_name, phone=phone, 
                             email=self.normalize_email(email.lower()),
-                            is_active=is_active, is_admin=is_admin, address=address
+                            is_active=is_active, is_admin=is_admin
                         )
 
         if password is not None:
@@ -38,7 +39,7 @@ class BaseUserManager(BUM):
 
     def create_superuser(
                         self, email,
-                        phone, password=None, address=None,
+                        phone, password=None,
                         first_name=None, last_name=None
                         ):
         """if not phone:
@@ -52,7 +53,6 @@ class BaseUserManager(BUM):
             is_active=True,
             is_admin=True,
             password=password,
-            address=address
         )
 
         user.is_superuser = True
@@ -74,13 +74,7 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
     
 
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    address = models.TextField(
-                                verbose_name="address",
-                                max_length=225,
-                                blank=True,
-                                null=True
-                            )
+    is_admin = models.BooleanField(default=False) 
 
    
 
@@ -96,6 +90,7 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
         return self.is_admin
 
 
+
 class Profile(models.Model):
     user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
 
@@ -103,7 +98,30 @@ class Profile(models.Model):
         return f"{self.user.email}"
 
 
+class ShippingAddress(BaseModel):
+    customer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="shipping_addresses", verbose_name=_("Customer"))
+    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
+    company = models.CharField(max_length=200, blank=True, verbose_name=_("Company"))
+    address = models.CharField(max_length=200, verbose_name=_("Address"))
+    city = models.CharField(max_length=100, verbose_name=_("City"))
+    state = models.CharField(max_length=100, verbose_name=_("State"))
+    postal_code = models.CharField(max_length=20, verbose_name=_("Postal Code"))
+    country = models.CharField(max_length=100, verbose_name=_("Country"))
+    phone = models.CharField(max_length=20, verbose_name=_("Phone"))
+    is_default = models.BooleanField(default=False, verbose_name=_("Default Address"))
 
+    class Meta:
+        verbose_name = _("Shipping Address")
+        verbose_name_plural = _("Shipping Addresses")
+
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            ShippingAddress.objects.filter(customer=self.customer, is_default=True).exclude(pk=self.pk).update(is_default=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.city}, {self.country}"
 
 
 

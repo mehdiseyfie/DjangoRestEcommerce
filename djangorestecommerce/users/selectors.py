224d72d718d2f20414fbd75564cabec982807a8b
Mehdi <mmmehdiseyfi@gmail.com>
2026-07-29 11:14:08 +0330

    add git shipping_address_by_id and get_shipping_addresses_by_profile

--- djangorestecommerce/users/selectors.py
+++ djangorestecommerce/users/selectors.py
@@ -1,4 +1,33 @@
-from .models import Profile, BaseUser
+from .models import Profile, BaseUser 
+from django.db.models import QuerySet 
+from djangorestecommerce.users.models import ShippingAddress
+from typing import Optional 
+from django.shortcuts import get_object_or_404
 
 def get_profile(user:BaseUser) -> Profile:
-    return Profile.objects.get(user=user)
+    return Profile.objects.get(user=user) 
+
+def get_shipping_addresses_by_profile(customer: Profile) -> QuerySet[ShippingAddress]:
+    """Get all shipping addresses for customer"""
+    return ShippingAddress.objects.filter(
+        customer=customer
+    ).order_by('-is_default', '-created_at') 
+    
+
+def get_default_shipping_address(customer: Profile) -> Optional[ShippingAddress]:
+    """Get default shipping address"""
+    try:
+        return ShippingAddress.objects.get(
+            customer=customer,
+            is_default=True
+        )
+    except ShippingAddress.DoesNotExist:
+        return None
+
+
+def get_shipping_address_by_id(address_id: int, customer: Profile) -> ShippingAddress:
+    """Get specific shipping address"""
+    return get_object_or_404(
+        ShippingAddress.objects.filter(customer=customer),
+        id=address_id
+    )
